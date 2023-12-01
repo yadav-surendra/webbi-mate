83e7d1c97e77f3a63d368d65be499b3de0254b25
yadav-surendra <138237820+yadav-surendra@users.noreply.github.com>
2023-12-01 20:03:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,67 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
-function firstpageanim(){
-    var t1= gsap.timeline();
+/*const inputBox = document.querySelector('.input-box');
+const searchBtn = document.getElementById('searchBtn');
+const weather_img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const town = document.querySelector('.town');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind-speed');
+const location_not_found = document.querySelector('.location-not-found');
 
-    t1.from("#nav", {
-        y: '-10',
-        opacity: 0,
-        duration: 1,
-        ease: Expo.easeInOut
-    })
+const weather_body = document.querySelector('.weather-body');
 
-    .to(".boundingelem", {
-        y: '0',
-        duration: 1.2,
-        ease: Expo.easeInOut,
-        stagger: .2
-    })
 
-    .from("#herofooter", {
-        y: '-10',
-        opacity: 0,
-        duration: 1,
-        delay: -.5,
-        ease: Expo.easeInOut
-    })
-    
+async function checkWeather(city){
+    const api_key = "7e8c44289eb7922b2c05519ff961f61a";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+    const weather_data = await fetch(`${url}`).then(response => response.json());
+
+
+    if(weather_data.cod === `404`){
+        location_not_found.style.display = "flex";
+        weather_body.style.display = "none";
+        console.log("error");
+        return;
+    }
+
+    console.log("run");
+    console.log(weather_data);
+    location_not_found.style.display = "none";
+    weather_body.style.display = "flex";
+    temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+    description.innerHTML = `${weather_data.weather[0].description}`;
+    town.innerHTML = `${weather_data.name}`;
+    humidity.innerHTML = `${weather_data.main.humidity}%`;
+    wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
+    cityName.innerHTML =`${weather_data.name}`;
+    cityName.style.visibility= "visible";
+    switch(weather_data.weather[0].main){
+        case 'Clouds':
+            weather_img.src = "cloud.png";
+            break;
+        case 'Clear':
+            weather_img.src = "clear.png";
+            break;
+        case 'Rain':
+            weather_img.src = "rain.png";
+            break;
+        case 'Mist':
+            weather_img.src = "mist.png";
+            break;
+        case 'Snow':
+            weather_img.src = "snow.png";
+            break;
+
+    }
+
+    console.log(weather_data);
 }
 
 
-function circleMousefollower() {
-    window.addEventListener("mousemove",function(dets){
-        this.document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
-    })
-};
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(inputBox.value);
+});
 
-circleMousefollower();
-firstpageanim();
+
+*/
